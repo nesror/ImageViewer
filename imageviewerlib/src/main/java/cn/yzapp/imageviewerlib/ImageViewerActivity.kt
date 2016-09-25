df4565b4e91a1e35ce112a17eb0e74a12c310082
nestor <nestor@yzapp.cn>
2016-09-25 21:06:31 +0800

    kotlin改写

---
+++ imageviewerlib/src/main/java/cn/yzapp/imageviewerlib/ImageViewerActivity.kt
@@ -0,0 +1,233 @@
+package cn.yzapp.imageviewerlib
+
+import android.animation.ValueAnimator
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.util.SparseArray
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_image_viewer.*
+
+import java.io.File
+
+import uk.co.senab.photoview.PhotoViewAttacher
+
+/**
+ * @author nestor
+ * *         email nestor@yzapp.cn
+ */
+class ImageViewerActivity : Activity() {
+
+    private var mShowImage: ImageInfo? = null
+    private var mLocalBroadcastManager: LocalBroadcastManager? = null
+    private var mSamplePagerAdapter: SamplePagerAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_image_viewer)
+
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this)
+        mShowImage = intent.getParcelableExtra<ImageInfo>(ImageViewer.INTENT_IMAGE)
+
+        setViewPage()
+        setOpenAnimator()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (isFinishing) {
+            overridePendingTransition(0, 0)
+        }
+    }
+
+    override fun onDestroy() {
+        val intent = Intent()
+        intent.action = ImageViewer.BROADCAST_ACTION
+        intent.putExtra("position", -1)
+        intent.putExtra("onDestroy", true)
+        mLocalBroadcastManager!!.sendBroadcast(intent)
+
+        super.onDestroy()
+    }
+
+    private fun setViewPage() {
+
+        mSamplePagerAdapter = SamplePagerAdapter()
+        view_pager.adapter = mSamplePagerAdapter
+
+        setCircleIndicator(view_pager)
+
+        view_pager.currentItem = mShowImage!!.index
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                sendBroadcast(position)
+                val smoothImageView = mSamplePagerAdapter!!.getPhotoView(position)
+                if (smoothImageView != null) {
+                    smoothImageView.scale = 1.0f
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+    }
+
+    private fun setCircleIndicator(mViewPager: HackyViewPager) {
+        val indicatorBackgroundId = intent.getIntExtra(ImageViewer.CHOOSE_RES_IS, 0)
+        val indicatorUnselectedBackgroundId = intent.getIntExtra(ImageViewer.UNCHOOSE_RES_IS, 0)
+        indicator.configureIndicator(-1, -1, -1, 0, 0, indicatorBackgroundId, indicatorUnselectedBackgroundId)
+
+        if (mShowImage!!.img.size > 1) {
+            indicator.visibility = View.VISIBLE
+            indicator.setViewPager(mViewPager)
+        }
+    }
+
+    private fun setOpenAnimator() {
+        // 进度动画
+        val animator = ValueAnimator.ofInt(0, 0xFF)
+        animator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Int
+            root!!.setBackgroundColor(progress * 0x1000000)
+        }
+        // 设置插值器为均速滚动
+        //animator.interpolator = LinearInterpolator()
+        animator.duration = 380
+        animator.start()
+    }
+
+    private fun setCloseAnimator() {
+        // 进度动画
+        val animator = ValueAnimator.ofInt(0xFF, 0)
+        animator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Int
+            root!!.setBackgroundColor(progress * 0x1000000)
+        }
+        // 设置插值器为均速滚动
+        //animator.interpolator = LinearInterpolator()
+        animator.duration = 380
+        animator.start()
+    }
+
+    private fun sendBroadcast(position: Int) {
+        val intent = Intent()
+        intent.action = ImageViewer.BROADCAST_ACTION
+        intent.putExtra("position", position)
+        mLocalBroadcastManager!!.sendBroadcast(intent)
+    }
+
+    internal inner class SamplePagerAdapter : PagerAdapter() {
+        private val photoViews: SparseArray<SmoothImageView>
+        private var show: Boolean = false
+
+        init {
+            photoViews = SparseArray<SmoothImageView>(mShowImage!!.img.size)
+        }
+
+        override fun getCount(): Int {
+            return mShowImage!!.img.size
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): View {
+            val photoView = SmoothImageView(container.context)
+            setImgSite(position, photoView)
+
+            if (position == mShowImage!!.index && !show) {
+                photoView.transformIn()
+                show = true
+            }
+            setPhotoView(photoView, mShowImage!!.img[position])
+
+            container.addView(photoView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+            photoViews.put(position, photoView)
+            return photoView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            container.removeView(`object` as View)
+            photoViews.remove(position)
+        }
+
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view === `object`
+        }
+
+        fun getPhotoView(position: Int): SmoothImageView? {
+            return photoViews.get(position)
+        }
+
+        /**
+         * 设置图片起始位置
+         */
+        private fun setImgSite(position: Int, photoView: SmoothImageView) {
+            var size: IntArray? = null
+            if (mShowImage!!.sizes.size > position) {
+                size = mShowImage!!.sizes[position]
+            }
+
+            if (size == null || size.size < 4) {
+                photoView.setOriginalInfo(
+                        0,
+                        0,
+                        Utils.getScreenWidth(this@ImageViewerActivity) / 2,
+                        Utils.getScreenHeight(this@ImageViewerActivity) / 2)
+            } else {
+                photoView.setOriginalInfo(
+                        size[0],
+                        size[1],
+                        size[2],
+                        size[3])
+            }
+        }
+
+
+        private fun setPhotoView(photoView: SmoothImageView, img: Any) {
+            if (img is String) {
+                ImageViewerConfig.imageLoader!!.getImage(this@ImageViewerActivity, photoView, img)
+            }
+            if (img is Int) {
+                ImageViewerConfig.imageLoader!!.getImage(this@ImageViewerActivity, photoView, img)
+            }
+            if (img is File) {
+                ImageViewerConfig.imageLoader!!.getImage(this@ImageViewerActivity, photoView, img)
+            }
+            if (img is Bitmap) {
+                ImageViewerConfig.imageLoader!!.getImage(this@ImageViewerActivity, photoView, img)
+            }
+
+            photoView.setOnTransformListener(object : SmoothImageView.TransformListener {
+                override fun onTransformComplete(mode: Int) {
+                    if (mode == 2) {
+                        finish()
+                    }
+                }
+            })
+
+            photoView.setOnPhotoTapListener(object : PhotoViewAttacher.OnPhotoTapListener {
+                override fun onPhotoTap(view: View, v: Float, v2: Float) {
+                    photoView.transformOut()
+                    setCloseAnimator()
+
+                }
+
+                override fun onOutsidePhotoTap() {
+                    photoView.transformOut()
+                    setCloseAnimator()
+                }
+            })
+        }
+
+    }
+
+}
--- imageviewerlib/src/main/java/cn/yzapp/imageviewerlib/MainActivity.kt
+++ imageviewerlib/src/main/java/cn/yzapp/imageviewerlib/MainActivity.kt
@@ -1,22 +1,34 @@
 package cn.yzapp.imageviewerlib
 
+import android.animation.ValueAnimator
+import android.content.Intent
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.util.SparseArray
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import kotlinx.android.synthetic.main.activity_image_viewer.*
 import uk.co.senab.photoview.PhotoViewAttacher
 import java.io.File
 
+@Deprecated("")
 class MainActivity : AppCompatActivity() {
 
-    var mImageInfo: ImageInfo? = null
+    private var mImageInfo: ImageInfo? = null
+    private var mSamplePagerAdapter: SamplePagerAdapter? = null
+    private var mLocalBroadcastManager: LocalBroadcastManager? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this)
+
         setContentView(R.layout.activity_image_viewer)
 
         mImageInfo = intent.getParcelableExtra<ImageInfo>(ImageViewer.INTENT_IMAGE)
@@ -27,22 +39,84 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setViewPage() {
+        mSamplePagerAdapter = SamplePagerAdapter(mImageInfo)
+        view_pager.adapter = mSamplePagerAdapter
+        setCircleIndicator(view_pager)
 
+        view_pager.currentItem = mImageInfo?.index ?: 0
+
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                sendBroadcast(position)
+                val smoothImageView = mSamplePagerAdapter?.getPhotoView(position)
+                if (smoothImageView != null) {
+                    smoothImageView.scale = 1.0f
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+    }
+
+    private fun sendBroadcast(position: Int) {
+        val intent = Intent()
+        intent.action = ImageViewer.BROADCAST_ACTION
+        intent.putExtra("position", position)
+        mLocalBroadcastManager?.sendBroadcast(intent)
+    }
+
+    private fun setCircleIndicator(view_pager: HackyViewPager?) {
+        val indicatorBackgroundId = intent.getIntExtra(ImageViewer.CHOOSE_RES_IS, 0)
+        val indicatorUnselectedBackgroundId = intent.getIntExtra(ImageViewer.UNCHOOSE_RES_IS, 0)
+        indicator.configureIndicator(-1, -1, -1, 0, 0, indicatorBackgroundId, indicatorUnselectedBackgroundId)
+
+        if (mImageInfo?.img?.size ?: 0 > 1) {
+            indicator.visibility = View.VISIBLE
+            indicator.setViewPager(view_pager)
+        }
     }
 
     private fun setOpenAnimator() {
+        // 进度动画
+        val animator = ValueAnimator.ofInt(0, 0xFF)
+        animator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Int
+            root.setBackgroundColor(progress * 0x1000000)
+        }
+        // 设置插值器为均速滚动
+        animator.interpolator = LinearInterpolator()
+        animator.duration = 300
+        animator.start()
+    }
+
+    private fun setCloseAnimator() {
+        // 进度动画
+        val animator = ValueAnimator.ofInt(0xFF, 0)
+        animator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Int
+            root.setBackgroundColor(progress * 0x1000000)
+        }
+        // 设置插值器为均速滚动
+        animator.interpolator = LinearInterpolator()
+        animator.duration = 300
+        animator.start()
     }
 
 
-    class SamplePagerAdapter : PagerAdapter {
+    inner class SamplePagerAdapter : PagerAdapter {
 
         var photoViews: SparseArray<SmoothImageView>? = null
         var mImageInfo: ImageInfo? = null
         var show = false
 
-        constructor(imageInfo: ImageInfo) {
+        constructor(imageInfo: ImageInfo?) {
             mImageInfo = imageInfo
-            photoViews = SparseArray<SmoothImageView>(imageInfo.img.size)
+            photoViews = SparseArray<SmoothImageView>(imageInfo?.img?.size ?: 0)
         }
 
         override fun instantiateItem(container: ViewGroup?, position: Int): Any {
@@ -64,9 +138,13 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        fun getPhotoView(position: Int): SmoothImageView? {
+            return photoViews?.get(position)
+        }
 
         override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
-            super.destroyItem(container, position, `object`)
+            container?.removeView(`object` as View)
+            photoViews?.remove(position)
         }
 
         override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
@@ -100,42 +178,43 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        private fun setPhotoView(photoView: SmoothImageView, img: Any?) {
-            if (img is String) {
-                ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
-            }
-            if (img is Int) {
-                ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
-            }
-            if (img is File) {
-                ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
-            }
-            if (img is Bitmap) {
-                ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
-            }
 
-            photoView.setOnTransformListener(object : SmoothImageView.TransformListener {
-                override fun onTransformComplete(mode: Int) {
-                    if (mode == 2) {
-                        this@MainActivity.finish()
-                    }
-                }
-            })
-
-            photoView.setOnPhotoTapListener(object : PhotoViewAttacher.OnPhotoTapListener {
-                override fun onPhotoTap(view: View, v: Float, v2: Float) {
-                    photoView.transformOut()
-                    setCloseAnimator()
-
-                }
-
-                override fun onOutsidePhotoTap() {
-                    photoView.transformOut()
-                    setCloseAnimator()
-                }
-            })
-        }
     }
 
+    fun setPhotoView(photoView: SmoothImageView, img: Any?) {
+        if (img is String) {
+            ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
+        }
+        if (img is Int) {
+            ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
+        }
+        if (img is File) {
+            ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
+        }
+        if (img is Bitmap) {
+            ImageViewerConfig.imageLoader!!.getImage(this@MainActivity, photoView, img)
+        }
+
+        photoView.setOnTransformListener(object : SmoothImageView.TransformListener {
+            override fun onTransformComplete(mode: Int) {
+                if (mode == 2) {
+                    this@MainActivity.finish()
+                }
+            }
+        })
+
+        photoView.setOnPhotoTapListener(object : PhotoViewAttacher.OnPhotoTapListener {
+            override fun onPhotoTap(view: View, v: Float, v2: Float) {
+                photoView.transformOut()
+                setCloseAnimator()
+
+            }
+
+            override fun onOutsidePhotoTap() {
+                photoView.transformOut()
+                setCloseAnimator()
+            }
+        })
+    }
 
 }
